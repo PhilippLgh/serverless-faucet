ad8da8145905f46f5d056eba5daeeab329ae8b65
PhilippLgh <philipp@ethereum.org>
2020-07-21 19:05:06 +0200

    Updates

---
+++ precache-manifest.4f83cb31b024f7c10ec50b07cc5fc5d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd2ae8aa004ceed79fddafd8ce6ffc74",
+    "url": "/serverless-faucet/index.html"
+  },
+  {
+    "revision": "0d50e9221c374fc85510",
+    "url": "/serverless-faucet/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "63c83a6f482a906a8f12",
+    "url": "/serverless-faucet/static/js/2.c47e44f6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/serverless-faucet/static/js/2.c47e44f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d50e9221c374fc85510",
+    "url": "/serverless-faucet/static/js/main.7352f37f.chunk.js"
+  },
+  {
+    "revision": "b833fbb6c4f6d0a4ea16",
+    "url": "/serverless-faucet/static/js/runtime-main.ec691d13.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/serverless-faucet/static/media/logo.5d5d9eef.svg"
+  }
+]);
